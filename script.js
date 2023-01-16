e42956875fc770ea524220479e24c97edc79fa85
syl1m <shengyaulim@gmail.com>
2023-01-16 15:52:40 +0800

    Add JS to check when the game is over: tie or win.

--- script.js
+++ script.js
@@ -22,8 +22,64 @@ const gameBoard = (() => {
 })();
 
 const playGame = (() => {
-  let currentPlayer = player1;
+  let gameState = "unresolved";
+  const checkWinState = () => {
+    if (
+      gameBoard.boardArray[0] === gameBoard.boardArray[1] &&
+      gameBoard.boardArray[0] === gameBoard.boardArray[2] &&
+      gameBoard.boardArray[0]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[3] === gameBoard.boardArray[4] &&
+      gameBoard.boardArray[3] === gameBoard.boardArray[5] &&
+      gameBoard.boardArray[3]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[6] === gameBoard.boardArray[7] &&
+      gameBoard.boardArray[6] === gameBoard.boardArray[8] &&
+      gameBoard.boardArray[6]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[0] === gameBoard.boardArray[3] &&
+      gameBoard.boardArray[0] === gameBoard.boardArray[6] &&
+      gameBoard.boardArray[0]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[1] === gameBoard.boardArray[4] &&
+      gameBoard.boardArray[1] === gameBoard.boardArray[7] &&
+      gameBoard.boardArray[1]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[2] === gameBoard.boardArray[5] &&
+      gameBoard.boardArray[2] === gameBoard.boardArray[8] &&
+      gameBoard.boardArray[2]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[0] === gameBoard.boardArray[4] &&
+      gameBoard.boardArray[0] === gameBoard.boardArray[8] &&
+      gameBoard.boardArray[0]
+    ) {
+      gameState = "win";
+    } else if (
+      gameBoard.boardArray[2] === gameBoard.boardArray[4] &&
+      gameBoard.boardArray[2] === gameBoard.boardArray[6] &&
+      gameBoard.boardArray[2]
+    ) {
+      gameState = "win";
+    } else if (!gameBoard.boardArray.includes(undefined)) {
+      gameState = "tie";
+    }
 
+    return gameState;
+  };
+
+  let currentPlayer = player1;
   const swapPlayerTurn = () => {
     if (currentPlayer === player1) {
       currentPlayer = player2;
@@ -32,14 +88,30 @@ const playGame = (() => {
     }
   };
 
+  const gameOver = () => {
+    if (gameState === "win") {
+      alert(`${currentPlayer.name} has won the game`);
+    } else {
+      alert("Tie game!");
+    }
+  };
+
   const placeMarker = (e) => {
+    if (gameState === "tie" || gameState === "win") return;
     const i = e.target.dataset.index;
     if (gameBoard.boardArray[i]) return;
     gameBoard.boardArray[i] = currentPlayer.marker;
     e.target.textContent = currentPlayer.marker;
-    swapPlayerTurn();
+    checkWinState();
+    if (gameState === "win" || gameState === "tie") {
+      gameOver();
+    } else {
+      swapPlayerTurn();
+    }
   };
 
   const tiles = document.querySelectorAll(".gameBoard div");
   tiles.forEach((tile) => tile.addEventListener("click", placeMarker));
+
+  return {};
 })();
